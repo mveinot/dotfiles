6b9edcc1df17dd5a3a558a7d24266d545c8a0254
Jason Harley <jason.harley@hypert.com>
2014-01-16 14:22:15 -0500

    Support bash_completion.d populated by 'brew'

--- system/_bash/075_bash_completion.sh
+++ system/_bash/075_bash_completion.sh
@@ -1,4 +1,9 @@
-if which brew > /dev/null
-then
-  if [ -f `brew --prefix`/etc/bash_completion ]; then source `brew --prefix`/etc/bash_completion ; fi
+if which brew > /dev/null; then
+    BASHCOMPLETE_DIR="$(brew --prefix)/etc/bash_completion.d"
+
+    if [ -d $BASHCOMPLETE_DIR ]; then
+        for f in $(ls $BASHCOMPLETE_DIR); do
+            source $BASHCOMPLETE_DIR/$f
+        done
+    fi
 fi
