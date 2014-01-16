babda4b1b17d1435e07e7c7d23beaf7850d18b66
Jason Harley <jason.harley@hypert.com>
2014-01-16 16:09:56 -0500

    Bugfix to not rely on 'ls'

--- system/_bash/075_bash_completion.sh
+++ system/_bash/075_bash_completion.sh
@@ -2,8 +2,8 @@ if which brew > /dev/null; then
     BASHCOMPLETE_DIR="$(brew --prefix)/etc/bash_completion.d"
 
     if [ -d $BASHCOMPLETE_DIR ]; then
-        for f in $(ls $BASHCOMPLETE_DIR); do
-            source $BASHCOMPLETE_DIR/$f
+         for f in $BASHCOMPLETE_DIR/*; do
+            source $f
         done
     fi
 fi
