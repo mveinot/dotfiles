2250e060a89db7950bf99dab8951ab76e821285d
Jeff MacDonald <jeff@terida.com>
2014-01-20 15:34:46 -0400

    updated java home for java 1.7

--- system/_bash/060_java.sh
+++ system/_bash/060_java.sh
@@ -1,3 +1,4 @@
-JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Home
+#JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Home
+JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.7.0_51.jdk/Contents/Home/
 
 [ -d $JAVA_HOME ] && export JAVA_HOME
